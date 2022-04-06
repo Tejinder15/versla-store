198788e56b108c324e5b39445cae95746cef7622
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-04-06 12:23:53 +0530

    feat: added dynamic cart button

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,59 +1,71 @@
 import { useState } from "react";
+import { useAuth } from "../../Context/AuthContext/auth-context";
+import { useCart } from "../../Context/CartContext/cart-context";
 import { useWishlist } from "../../Context/WishContext/wishlist-context";
+import { useNavigate } from "react-router-dom";
 const ProductCard = (props) => {
-  const { wishlistState, wishlistDispatch } = useWishlist();
-  const { wishlist } = wishlistState;
-  const checkInWishlist = (id) => {
-    const item = wishlist.find((item) => item._id === id);
-    return item ? true : false;
-  };
-  const [isinWishlist, setIsInWishlist] = useState(
-    checkInWishlist(props.productId)
-  );
+  const {
+    wishlistState: { wishlist },
+    wishlistDispatch,
+  } = useWishlist();
+  const {
+    cartState: { cart },
+  } = useCart();
+  const navigate = useNavigate();
   return (
     <>
       <div className="basic-card">
         <div className="basic-card-image-title badge-image-title">
           <div className="product-image-container">
             <img
-              src={props.productImg}
-              alt={props.productTitle}
+              src={props.productDetail.image}
+              alt={props.productDetail.title}
               className="basic-card-image productcard_image"
             />
           </div>
           <span className="dot">
-            <span
-              className={`material-icons-outlined ${
-                isinWishlist && "heart-filled"
-              }`}
-              onClick={() => {
-                return isinWishlist
-                  ? props.removeFromWishlist(props.productId, setIsInWishlist)
-                  : props.addToWishlistHandler(
-                      props.productId,
-                      setIsInWishlist
-                    );
-              }}
-            >
-              {isinWishlist ? "favorite" : "favorite_border"}
-            </span>
+            {wishlist.some((item) => item._id === props.productDetail._id) ? (
+              <span
+                className={"material-icons-outlined heart-filled"}
+                onClick={() =>
+                  props.removeFromWishlistHandler(props.productDetail._id)
+                }
+              >
+                favorite
+              </span>
+            ) : (
+              <span
+                className={"material-icons-outlined"}
+                onClick={() => props.addToWishlistHandler(props.productDetail)}
+              >
+                favorite_border
+              </span>
+            )}
           </span>
         </div>
         <div className="basic-card-details">
           <div className="basic-card-secondary">
-            <p className="product-title center-text">{props.productTitle}</p>
+            <p className="product-title center-text">
+              {props.productDetail.title}
+            </p>
             <h3 className="product-price center-text">
-              &#8377;{props.productPrice}
+              &#8377;{props.productDetail.price}
             </h3>
           </div>
         </div>
         <div className="basic-card-actions">
-          <button
-            className="move-to-cart"
-            onClick={() => props.addToCart(props.productId)}
-          >
-            Add to Cart
-          </button>
+          {cart.some((item) => item._id === props.productDetail._id) ? (
+            <button className="move-to-cart" onClick={() => navigate("/cart")}>
+              Go to Cart
+            </button>
+          ) : (
+            <button
+              className="move-to-cart"
+              onClick={() => props.addToCart(props.productDetail)}
+            >
+              Add to Cart
+            </button>
+          )}
         </div>
       </div>
     </>
